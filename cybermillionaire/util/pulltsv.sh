f845962de558179333176afdadf9b02e8407207d
Ryne Krueger <rnkrueger9@gmail.com>
2020-10-09 16:27:41 -0400

    First Commit

---
+++ cybermillionaire/util/pulltsv.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# pulls our google sheet as a tsv
+wget "https://docs.google.com/spreadsheets/d/1wAyyionpvedzrM_rh33iLVX7hPssNF1K3U2WTCi_qLs/export?format=tsv" -O /Users/rynekrueger/Documents/CollegeWork/Cybercorps/CyberMillionaire/millionaire-env/cybermillionaire/cybermillionaire/util/complete_millionaire.tsv
+
