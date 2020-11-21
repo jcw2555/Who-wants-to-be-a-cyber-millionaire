4323bce2767beecee04235678fd8166bed7b3c0e
Ryne Krueger <rnkrueger9@gmail.com>
2020-11-20 21:05:44 -0600

    Initial Commit
    A script that checks every question for suspicious entries. It will then put any suspicious entries into the text file. A suspicious entry is any entry that has a semi-colon in it or if the rank entry is not a number between 1-20.

---
+++ cybermillionaire/util/verifyEntries.py
@@ -0,0 +1,45 @@
+"""
+Goes through the complete_millionaire.tsv file. It checks for any abnormal entries. If any are found, it will put that entry into another file.
+"""
+
+#turns list into a string to write into the suspicious entries file
+def makeString(list, row):
+    newstr = str(row) + ": \t "
+    for i in list:
+        newstr += i
+        newstr += " "
+
+    return newstr
+
+
+
+def main():
+    tsv = open("cybermillionaire/util/complete_millionaire.tsv", "r")
+    alert = open("cybermillionaire/util/susEntries.txt", "w+")
+
+    lines = tsv.readline()
+    # gets rid of header line in file
+    count = 1
+    lines = tsv.readline()
+    while (lines):
+        # print("lines: " , lines);
+        lineList = lines.split("\t")
+
+        for i in range(len(lineList)):
+            if(i == 6):
+                if(int(lineList[i]) < 0 or int(lineList[i]) > 20):
+                    alert.write(makeString(lineList, count))
+            else:
+                for letter in lineList[i]:
+                    if (letter == ';' or letter == "'"):    #checks for semi-colon or apostrophe
+                        alert.write(makeString(lineList, count))
+                        break;
+
+        lines = tsv.readline()
+        count +=1
+
+
+main()
+
+
+
