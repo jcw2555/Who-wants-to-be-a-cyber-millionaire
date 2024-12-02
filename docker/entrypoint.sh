a4bcd6d7227cffb03f71757ec032bae85925f518
Jenna Weinman <jcw2555@rit.edu>
2024-12-02 14:15:13 -0500

    got openai temp key in container, also got api calls working from within container

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 
+# Install necessary packages and Python dependencies
+apk add --no-cache python3 py3-pip
+pip3 install --upgrade pip
+pip3 install openai==0.26.0
+apk add --no-cache build-base python3-dev
+python3 -m pip install --upgrade openai
+
 rm cybermillionaire/__init__.py
 touch cybermillionaire/__init__.py
 
