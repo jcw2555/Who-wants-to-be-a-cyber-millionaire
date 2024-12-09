631c94100ab338f3e7476b61195c1b7e8a7182ab
Jenna Weinman <jcw2555@rit.edu>
2024-12-09 17:40:23 -0500

    cleaned up comments from the previous group

--- docker/cybermillionaire/cybermillionaire/export.py
+++ docker/cybermillionaire/cybermillionaire/export.py
@@ -9,30 +9,9 @@ import cybermillionaire.database_insert as insert
 def run_sql(cursor, query):
     cursor.execute(query)
     records = cursor.fetchall() #All the records for that query are here.
-    #print(records)
-
-    #print("Total rows: ", cursor.rowcount)
-
-    #print("\nPrinting each record")
-    
-    #Check rows, don't run this if there are thousands of entries.
-    #This is how we will read in values though
-    """
-    for row in records:
-        print("Question = ", row[0], "\n")
-        print("Ans1 = ", row[1], "\n")
-        print("Ans2 = ", row[2], "\n")
-        print("Ans3 = ", row[3], "\n")
-        print("Ans4 = ", row[4], "\n")
-        print("Correct = ", row[5], "\n")
-        print("Rank = ", row[6], "\n")
-    """
     return records
     
 def generate_json(game):
-    #data = {}
-    #data['games'] = []
-    #questions_json = "questions: "
     json_file = {}
     json_file['games'] = []
     questions = []
@@ -44,28 +23,19 @@ def generate_json(game):
         for i in range(len(question)):
             # Need to include "questions" around the individual question
             if i == 0:
-                #print("Question: " + question[i])
                 quest_json = question[i]
-            #Print "content" here before answers
             # Surround answers with [],
             if i == 1:
-                #print("Ans1: " + question[i])
                 ans1_json = question[i]
             if i == 2:
-                #print("Ans2: " + question[i])    
                 ans2_json = question[i]
             if i == 3:
-                #print("Ans3: " + question[i])    
                 ans3_json = question[i]
             if i == 4:
-                #print("Ans4: " + question[i])    
                 ans4_json = question[i]
             if i == 5:
-                #print("Correct: " + str(question[i]))    
                 correct_json = question[i]
-                     
-            #print("\n")
-            
+                                 
         #create content array
         content = []
         content.append(str(ans1_json))
@@ -81,52 +51,8 @@ def generate_json(game):
         })
     json_file['games'].append({"questions" : questions})
 
-    #with open('cybermillionaire/util/millionaire.json','w') as outfile:
     with open('static/js/millionaire.json','w') as outfile:
         json.dump(json_file, outfile)
-
-    # # Clear the dynamic table
-    # try:
-    #     cursor.execute("DELETE FROM dynamic;")  # Removes all rows from the table
-    #     cursor.connection.commit()  # Commit the transaction
-    #     print("Dynamic table cleared successfully!")
-    # except Exception as e:
-    #     print(f"An error occurred while clearing the table: {e}")
-
-""" 
-        data['games'].append({
-            #Need to include "questions" for all questions
-
-            "question": str(quest_json),
-            "content: [ Ans1": str(ans1_json),
-            "Ans2": str(ans2_json),
-            "Ans3": str(ans3_json),
-            "Ans4": str(ans4_json),
-            # Don't think we should string this, pretty sure json reads it as int
-            "] " + "Correct": int(correct_json)
-            
-        })
-"""
-# I think I took care of the two TODO before this comment with the function below? Unless I'm confused
-# Making this basic so we have it here, assuming we will have a variable 1-4 for level choice
-# This will change a bit when we are getting info from JS not user input (this should solve second TODO)
-"""def LevelSelect(selection):
-
-    if selection == '1':
-        K_8th()
-
-    elif selection == '2':
-        HighSchool()
-
-    elif selection == '3':
-        College_NonTech()
-
-    elif selection == '4':
-        College_Tech()
-
-    else:
-        print("Invalid Level Selection!")
-""" 
 
 # This will run when a Static Primary School game is selected. It will gather all questions for the game       
 def Static_Primary_School(cursor):
@@ -135,10 +61,10 @@ def Static_Primary_School(cursor):
     # Gets the 5 easy questions for the Static Primary School game
     sql_Primary_T1 = "select Question, Ans1, Ans2, Ans3, Ans4, Correct, Difficulty, Level FROM millionaire WHERE Difficulty = 'easy' AND Level = 1 ORDER BY rand() LIMIT 5;"
     result = run_sql(cursor, sql_Primary_T1)
+    #save everything as variables
     for row in result:
         game.append(row)
 
-    #save everything as variables
         
     # Gets the 5 medium questions for the Static Primary School game
     sql_Primary_T2 = "select Question, Ans1, Ans2, Ans3, Ans4, Correct, Difficulty, Level FROM millionaire WHERE Difficulty = 'medium' and Level = 1 ORDER BY rand() LIMIT 5;"
@@ -159,7 +85,6 @@ def Static_Primary_School(cursor):
 
 # This will run when a Static Secondary School game is selected. It will gather all questions for the game
 def Static_Secondary_School(cursor):
-
     game = []
 
     # Gets the 5 easy questions for the Static Secondary School game
